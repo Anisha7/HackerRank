e6b3369defbc7a313f6bf468969e989235ce41fc
Anisha Jain <anishajain@outlook.com>
2019-01-02 00:42:59 -0800

    newYearChaos bug fixes

--- interview-prep/arrays/newYearChaos.py
+++ interview-prep/arrays/newYearChaos.py
@@ -9,91 +9,62 @@ import random
 import re
 import sys
 
-# checks if two lists are equal
-def isEqual(l1, l2):
-    if (len(l1) != len(l2)):
-        return False
 
-    for i in range(len(l1)):
-        if (l1[i] != l2[i]):
-            return False
+def swap(q, i, j):
+    if (j >= len(q)):
+        return q
+    temp = q[i]
+    q[i] = q[j]
+    q[j] = temp
+    return q
     
-    return True
-
-# False if bribes > 2 for any elem in dict
-def checkBribeCount(d):
-    for k in d.keys():
-        if (d[k] > 2):
-            return False
-    return True
-
-# return total bribes made
-def countBribes(d):
+def place(q, start, i):
+    
+    # find q[i] in start
+    index = start.index(q[i])
+    # place it at i and count swaps
     count = 0
-    for k in d.keys():
-        count += d[k]
-    return count
+    dx = 1
+    if (i < index):
+        dx = -1
+    while (q[i] != start[i]):
+        # check for index errors
+        if (index + dx < 0 or index + dx >= len(q)):
+            print('index error, exiting loop, status: (q, q[i], start[i]) ')
+            print(q, q[i], start[i])
+            break
+        # swap places with index and index +/- 1
+        start = swap(start, index, index+dx)
+        count += 1
+        index += dx
+    if (count > 2):
+        print('Too chaotic')
+        count = None
+    return (count, start)
 
 # Complete the minimumBribes function below.
 def minimumBribes(q):
-    bribes = dict() #track how many bribes
-    # initialize dict
-    for i in range(len(q)):
-        bribes[i] = 0
+    count = 0
+    # starting position
+    start = list(range(1, len(q)+1))
     
-    start = list(range(1, len(q) + 1))
-    
-    i = len(q) - 1
-    while (isEqual(start,q) == False):
-        # prevent index out of range
-        if (i < 0):
-            i = len(q) - 1
-        if (q[i] != start[i]):
-            # swap
-            temp = start[i]
-            start[i] = start[i-1]
-            start[i-1] = temp
-            # add count
-            bribes[i] += 1
-        
-        # increment
-        i -= 1
-
-        # check if invalid
-        if (checkBribeCount(bribes) == False):
-            print('Too chaotic')
-            return
-
-    print(countBribes(bribes))
-    return
-
-# return index for where num is in q
-def find(q, num):
+    # check each element in list and place it at the right location
     for i in range(len(q)):
-        if (q[i] == num):
-            return i
-    return -1
-
-def minimumBribes2(q):
-    bribes = dict() #track how many bribes
-    # initialize dict
-    for i in range(len(q)):
-        num = i+1
-        index = find(q, num)
-        if (index != -1):
-            bribes[i] = abs(i-index)
-        else:
-            bribes[i] = 0
+        tup = place(q, start, i)
+        if (tup[0] == None):
+            count = None
+            break
+        count += tup[0]
+        start = tup[1]
     
-    if (checkBribeCount(bribes) == False):
-        print('Too chaotic')
-        return
-
-    print(countBribes(bribes))
-    return
+    if (count != None):
+        print(count)
+    return count
 
 if __name__ == '__main__':
     q = [2, 1, 5, 3, 4]
-    minimumBribes(q)
+    print('RESULT: ')
+    print(minBribes(q))
     q = [2, 5, 1, 3, 4]
-    minimumBribes(q)
+    print('RESULT: ')
+    print(minBribes(q))
