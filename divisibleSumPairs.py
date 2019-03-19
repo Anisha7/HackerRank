a3398a8378edb700f1069735012d11e9982d66b0
Anisha Jain <anishajain@outlook.com>
2019-03-19 01:04:29 -0700

    divisible sum pairs

---
+++ divisibleSumPairs.py
@@ -0,0 +1,10 @@
+# Complete the divisibleSumPairs function below.
+def divisibleSumPairs(n, k, ar):
+    count = 0
+    for i in range(n):
+        for j in range(i+1, n):
+            if ((ar[i] + ar[j]) % k == 0):
+                count += 1
+    return count
+
+print(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]))
